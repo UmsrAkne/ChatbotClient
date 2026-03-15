24b65829fd100b74bcce2639176c141c845a0f5a
UmsrAkne <sibata486@gmail.com>
2026-03-15 16:18:31 +0900

    init

---
+++ ChatbotClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ChatbotClient.Utils;
+using Prism.Mvvm;
+
+namespace ChatbotClient.ViewModels;
+
+public class MainWindowViewModel : BindableBase
+{
+    private readonly AppVersionInfo appVersionInfo = new ();
+
+    public string Title => appVersionInfo.Title;
+}
